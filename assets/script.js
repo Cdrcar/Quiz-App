38217b031d9bbd0eefae538ec6c631fd024fc348
Carmela <carmela881@outlook.com>
2023-02-23 21:17:24 +0000

    modified css

--- assets/script.js
+++ assets/script.js
@@ -1,86 +1,76 @@
-// variables for page elements
-// time and score
-let timeEl = document.querySelector("p.time");
-let secondsLeft = 75;
-let scoreEl = document.querySelector("#score");
-
-// sections
-// section intro
+// Intro
 const introEl = document.querySelector("#intro");
 
-// section questions
-//question section
+
+//Questions
 const questionsEl = document.querySelector("#questions");
-//where question goes
+
 let questionEl = document.querySelector("#question");
-// how many questions they have answered
+
 let questionCount = 0;
-// div yesornot
+
 const yesornotEl = document.querySelector("#yesornot");
 
-// section final
 const finalEl = document.querySelector("#final");
-// user initials
+
 let initialsInput = document.querySelector("#initials");
 
-// section highscores
 const highscoresEl = document.querySelector("#highscores");
-// ordered list
+
 let scoreListEl = document.querySelector("#score-list");
-// array of scores
+
 let scoreList = [];
 
-// buttons
-// start
+// Score and Time
+let timeEl = document.querySelector("p.time");
+let secondsLeft = 80;
+let scoreEl = document.querySelector("#score");
+
+// Buttons
+
 const startBtn = document.querySelector("#start");
 
-// answer button class
 const ansBtn = document.querySelectorAll("button.ansBtn")
-// answer1
+
 const ans1Btn = document.querySelector("#answer1");
-// answer2
+
 const ans2Btn = document.querySelector("#answer2");
-// answer3
+
 const ans3Btn = document.querySelector("#answer3");
-// answer4
+
 const ans4Btn = document.querySelector("#answer4");
-// submit-score
+
 const submitScrBtn = document.querySelector("#submit-score");
-// goback
+
 const goBackBtn = document.querySelector("#goback");
-// clearscores
+
 const clearScrBtn = document.querySelector("#clearscores");
-// view-scores
+
 const viewScrBtn = document.querySelector("#view-scores");
 
-// Object for question, answer, true/false
-const questions = [ // array of objects
+// Array of Objects
+const questions = [ 
     {
-        // question 0
         question: "Commonly used data types do NOT include:",
         answers: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
         correctAnswer: "2"
     },
     {
-        // question 1
         question: "The condition in an if / else statement is enclosed within ____.",
         answers: ["1. quotes", "2. curly brackets", "3. parentheses", "4. square brackets"],
         correctAnswer: "2"
     },
     {
-        // question 2
         question: "Arrays in Javascript can be used to store ____.",
         answers: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
         correctAnswer: "3"
     },
     {
-        // question 3
         question: "String values must be enclosed within ____ when being assigned to variables.",
         answers: ["1. commmas", "2. curly brackets", "3. quotes", "4. parentheses"],
         correctAnswer: "2"
     },
     {
-        // question 4
         question: "A very useful tool used during development and debugging for printing content to the debugger is:",
         answers: ["1. Javascript", "2. terminal/bash", "3. for loops", "4. console.log"],
         correctAnswer: "3"
@@ -105,7 +95,7 @@ function setTime() {
     }, 1000);
 }
 
-// start quiz with timer and set up questions
+// Start Quiz
 function startQuiz() {
     introEl.style.display = "none";
     questionsEl.style.display = "block";
@@ -115,7 +105,7 @@ function startQuiz() {
     setQuestion(questionCount);
 }
 
-// function to set question; takes in a count and displays the next question/answers
+// Questions
 function setQuestion(id) {
     if (id < questions.length) {
         questionEl.textContent = questions[id].question;
@@ -126,7 +116,7 @@ function setQuestion(id) {
     }
 }
 
-// function to check answer and then move to next question
+// Check answer and move to next question
 function checkAnswer(event) {
     event.preventDefault();
 
@@ -233,7 +223,7 @@ goBackBtn.addEventListener("click", function () {
 // Clear the scores
 clearScrBtn.addEventListener("click", clearScores);
 
-// View/Hide High Scores Button
+// View or Hide High Scores Button
 viewScrBtn.addEventListener("click", function () {
     introEl.style.display="none";
     if (highscoresEl.style.display === "none") {
